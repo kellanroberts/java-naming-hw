5bc95340e7d353473315d0b4f21e78359ac57ecd
kellanroberts <73409978+kellanroberts@users.noreply.github.com>
2020-11-20 18:17:17 -0500

    Add files via upload

---
+++ Stuff.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class stuff {public static void main(String[] args) {
+    // I'm going to name some stuff wrong
+
+    int valueOfPi = 3.141592;
+
+    char favoriteFood = "Mixed Shawarma";
+
+    boolean age = 39;
+}
+}
+
+
+
+
